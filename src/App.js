cb5ebd187dd866c243f76fe18a99cfd14d622ba0
rilex001 <ristic1900@gmail.com>
2020-06-11 18:32:39 +0200

    Heade

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
-import Button from './components/ButtonComponent'
+import { Grid } from '@material-ui/core'
+import Header from './components/Header';
 
 function App() {
   
 
   return (
     <>
-    <Button />
+      <Grid container direction='column'>
+        <Grid item>
+          <Header />
+        </Grid>
+        <Grid item container>
+          <Grid item xs={0} sm={2} />
+            <Grid item xs={12} sm={8}>
+              This is where the content will be
+            </Grid>
+          <Grid item xs={0} sm={2} />
+        </Grid>
+      </Grid>
+
     
     </>
   );
